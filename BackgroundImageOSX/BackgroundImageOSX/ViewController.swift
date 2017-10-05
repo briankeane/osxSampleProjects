249afad8517aae2573f6ecf2cf0d2fd284bf1a31
briankeane <lonesomewhistle@gmail.com>
2017-10-05 11:07:52 -0500

    initialize background image

---
+++ BackgroundImageOSX/BackgroundImageOSX/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BackgroundImageOSX
+//
+//  Created by Brian D Keane on 10/5/17.
+//  Copyright © 2017 Brian D Keane. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
